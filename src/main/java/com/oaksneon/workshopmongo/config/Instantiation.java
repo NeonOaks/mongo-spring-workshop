7796bdc130c515f1309ab1d0eec09895015da706
NeonOaks <davi.oaks@gmail.com>
2024-06-26 21:53:58 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/oaksneon/workshopmongo/config/Instantiation.java
+++ src/main/java/com/oaksneon/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.oaksneon.workshopmongo.domain.Post;
 import com.oaksneon.workshopmongo.domain.User;
 import com.oaksneon.workshopmongo.dto.AuthorDTO;
+import com.oaksneon.workshopmongo.dto.CommentDTO;
 import com.oaksneon.workshopmongo.repository.PostRepository;
 import com.oaksneon.workshopmongo.repository.UserRepository;
 
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null,new AuthorDTO(maria) ,sdf.parse("21/03/2018"),"Partiu viagem", "Vou viajar para São Paulo. Abraços!");
 		Post post2 = new Post(null,new AuthorDTO(maria),sdf.parse("21/03/2018"),"Bom dia", "Acordei feliz hoje!");
 				
+		CommentDTO c1 = new CommentDTO("Boa viajem mano!",sdf.parse("21/03/2018"),new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveita.....",sdf.parse("25/03/2018"),new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!!!!",sdf.parse("28/03/2018"),new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
