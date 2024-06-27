be0d0203f912399a8951ee94586c3e834275455f
NeonOaks <davi.oaks@gmail.com>
2024-06-26 21:30:18 -0300

    Obtendo um post por id

---
+++ src/main/java/com/oaksneon/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.oaksneon.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.oaksneon.workshopmongo.domain.Post;
+import com.oaksneon.workshopmongo.repository.PostRepository;
+import com.oaksneon.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+}
