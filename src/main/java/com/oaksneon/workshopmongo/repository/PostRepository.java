a8a2423fb734a0df828e751a219ab8c8f4412279
NeonOaks <davi.oaks@gmail.com>
2024-07-03 20:49:15 -0300

    Consulta simples com @Query

--- src/main/java/com/oaksneon/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/oaksneon/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.oaksneon.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.oaksneon.workshopmongo.domain.Post;
@@ -11,5 +12,7 @@ import com.oaksneon.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository  extends MongoRepository<Post, String>{
 	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> seachTitle(String text);
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
